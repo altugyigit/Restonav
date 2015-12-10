d391e7a0a1a1bd416725a80804554aa461de935d
altugyigit <altug_yigit@hotmail.com>
2015-12-10 16:20:35 +0200

    Main Actitivty Tasarım Rev

--- app/src/main/java/com/altygtsoft/restonav/MainActivity.java
+++ app/src/main/java/com/altygtsoft/restonav/MainActivity.java
@@ -1,11 +1,16 @@
 package com.altygtsoft.restonav;
 
-import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,25 +18,57 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
+    public void exitFunc()
+    {
+        final AlertDialog.Builder adialogBuilder = new AlertDialog.Builder(this);
+
+        adialogBuilder.setTitle("Çıkış");
+        adialogBuilder.setMessage("Çıkmak istiyor musunuz ?");
+        adialogBuilder.setPositiveButton("Evet", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                MainActivity.this.finishAffinity();
+
+            }
+        });
+        adialogBuilder.setNegativeButton("Hayır", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                dialog.cancel();
+
+            }
+        });
+
+        final AlertDialog adialog = adialogBuilder.create();
+        adialog.show();
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        int _id = item.getItemId();
+
+        if (_id == R.id.itemCancel)
+        {
+            exitFunc();//Cikis
+        }
+        if(_id == R.id.itemInfo)
+        {
+            Toast _toast = Toast.makeText(MainActivity.this, "Portaloji Yazılım\nGörüşleriniz ve önerileriniz için bilgi@portaloji.com", Toast.LENGTH_LONG);
+            TextView _tw = (TextView) _toast.getView().findViewById(android.R.id.message);
+            if( _tw != null) _tw.setGravity(Gravity.CENTER);
+            _toast.show();
         }
 
         return super.onOptionsItemSelected(item);
+
     }
 }
